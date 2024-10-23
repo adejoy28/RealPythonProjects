34920d889db7c035e19705d04360a220aeb39126
adejoy28 <adejoy28@gmail.com>
2024-10-23 17:40:18 +0100

    n

--- PDF-to-AudioBook/index.py
+++ PDF-to-AudioBook/index.py
@@ -1,3 +1,16 @@
 # install and import Python Modules
 # PyPDF2
 # pyttsx3
+import PyPDF2
+import pyttsx3
+
+
+Engine = pyttsx3.init()
+PDF_Reader = PyPDF2.PdfFileReader(open("../What is python.pdf", "rb"))
+
+for Page_num in range(PDF_Reader.numPages):
+    Text = PDF_Reader.getPage(Page_num).extractText()
+    # Engine.getProperty("voices")
+    Engine.say(Text)
+    Engine.runAndWait()
+
--- barcodeProjects/BarCode.py
+++ barcodeProjects/BarCode.py
@@ -1,5 +1,7 @@
 # First install python barcode module
 
+# pip install python-barcode
+
 from barcode import EAN13
 from barcode.writer import ImageWriter
 
